c9b377ec7c62dbef0b61efbd95419af865001c49
João Andrade <j_diogo_@hotmail.com>
2015-10-03 17:29:58 +0100

    Update MainScoreTester.java
    Adding some comments.
    Simplifying the sysout call to use the hsm toString()

--- src/pt/iscte/lei/pi/firujo/scores/MainScoreTester.java
+++ src/pt/iscte/lei/pi/firujo/scores/MainScoreTester.java
@@ -1,5 +1,8 @@
 package pt.iscte.lei.pi.firujo.scores;
-
+/*
+*	Esta classe serve exclusivamente para testar a lista de highscores.
+*	Não corram isto quando o trabalho estiver pronto ou podem estragar a lista de scores actual.
+*/
 public class MainScoreTester {
 
 	public static void main(String[] args) {
@@ -11,7 +14,7 @@ public class MainScoreTester {
 		hsm.addScore(new Score("jesus", 999));
 		hsm.addScore(new Score("zrawraw", 12412));
 		
-		System.out.println(hsm.getHighscoreString());
+		System.out.println(hsm);
 		
 	}
 }
