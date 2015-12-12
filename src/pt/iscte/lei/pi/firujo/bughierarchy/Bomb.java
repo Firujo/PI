a0c087a3a18b499805938fd7f0ad78be05ff050b
jdandrade <j_diogo_@hotmail.com>
2015-12-12 16:41:07 +0000

    bombs vidas sounds

---
+++ src/pt/iscte/lei/pi/firujo/bughierarchy/Bomb.java
@@ -0,0 +1,9 @@
+package pt.iscte.lei.pi.firujo.bughierarchy;
+
+public class Bomb extends Animal{
+
+	public Bomb(int lvl) {
+		super(lvl);
+	}
+
+}
---
+++ src/pt/iscte/lei/pi/firujo/bughierarchy/Hp.java
@@ -0,0 +1,9 @@
+package pt.iscte.lei.pi.firujo.bughierarchy;
+
+public class Hp extends Animal{
+
+	public Hp(int lvl) {
+		super(lvl);
+	}
+
+}
